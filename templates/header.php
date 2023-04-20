616a5e601e1a2cafcdae35e2365814bcb552a051
Guaidot <122104260+Guaidot@users.noreply.github.com>
2023-04-19 21:42:08 -0400

    Add files via upload

---
+++ templates/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CONTROL Y SEGUIMIENTO</title>
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+	<div class="dropdown">
+  <ul class="nav nav-pills">
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">INGRESAR</a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="planificada.php">PLANIFICADAS</a>
+        <a class="dropdown-item" href="ejecutadas.php">EJECUTADAS</a>
+        <a class="dropdown-item" href="reprogramadas.html">REPROGRAMADAS</a>
+      </div>
+    </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">MODIFICAR</a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="prueba.php">PLANIFICADAS</a>
+        <a class="dropdown-item" href="ejecutadas.php">EJECUTADAS</a>
+        <a class="dropdown-item" href="reprogramadas.html">REPROGRAMADAS</a></div>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">BUSCAR</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link " href="eliminar.php">ELIMINAR</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">REPORTE</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="index.php">SALIR</a>
+    </li>
+  </ul>
+</div>
+
+</body>
+</html>
